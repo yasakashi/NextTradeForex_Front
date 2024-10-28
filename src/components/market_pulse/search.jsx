1c907e439a2c39277cb6a914ee3b12975bac9948
Mahdi <mahdiuser100@gmail.com>
2024-10-28 21:51:16 +0330

    feat: market pulse search input

---
+++ src/components/market_pulse/search.jsx
@@ -0,0 +1,14 @@
+import MultiSelect from "../MultiSelect";
+
+function Search() {
+  return (
+    <div className="mx-auto mt-[30px] z-[1]">
+      <input
+        placeholder="Search a lesson"
+        className="bg-gold-light_200 w-full placeholder:text-black px-[3px] py-[2px]"
+      />
+    </div>
+  );
+}
+
+export default Search;
