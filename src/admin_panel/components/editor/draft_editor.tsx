b9d45a3afaf0fea7725335f0cb53e14c23fdb5e0
Mahdi <mahdiuser100@gmail.com>
2024-10-28 21:37:13 +0330

    feat: allow initial data

--- src/admin_panel/components/editor/draft_editor.tsx
+++ src/admin_panel/components/editor/draft_editor.tsx
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Editor } from "react-draft-wysiwyg";
-import { convertFromHTML, convertToRaw, EditorState } from "draft-js";
+import { convertFromHTML, EditorState, ContentState } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
 const DraftEditor = ({
   set_editor_value,
   editorState,
+  initialContent,
 }: {
   editorState?: EditorState;
   set_editor_value?: (val: EditorState) => void;
+  initialContent?: string;
 }) => {
+  const [editorStateLocal, setEditorStateLocal] = useState<EditorState>(editorState || EditorState.createEmpty());
+
+  useEffect(() => {
+    if (initialContent) {
+      // Convert the initial content from HTML to ContentState
+      const blocksFromHTML = convertFromHTML(initialContent);
+      const contentState = ContentState.createFromBlockArray(blocksFromHTML.contentBlocks, blocksFromHTML.entityMap);
+      const newEditorState = EditorState.createWithContent(contentState);
+      setEditorStateLocal(newEditorState);
+      set_editor_value?.(newEditorState);
+    }
+  }, [initialContent, set_editor_value]);
+
   return (
     <div
       style={{
         width: "100%",
-        minHeight: 400,
+        maxHeight: 400,
         backgroundColor: "white",
         color: "black",
         borderRadius: 8,
         padding: 8,
+        overflow: "auto",
+        border: "1px solid gray",
       }}
     >
       <Editor
-        editorState={editorState}
+        editorState={editorStateLocal}
         toolbarClassName="toolbarClassName"
         wrapperClassName="wrapperClassName"
         editorClassName="editorClassName"
@@ -29,10 +47,8 @@ const DraftEditor = ({
         spellCheck
         toolbar={{}}
         onEditorStateChange={(state) => {
-          // let row = convertToRaw(state.getCurrentContent())
-          //   .blocks.map((item) => item.text)
-          //   .join(" ");
           set_editor_value?.(state);
+          setEditorStateLocal(state);
         }}
       />
     </div>
