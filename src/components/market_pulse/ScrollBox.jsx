2f864177762f39867937df14dd29722b16551a73
Mediv0 <mahdiuser100@gmail.com>
2024-11-11 11:27:49 +0330

    fix: invalid Draftjs State

--- src/components/market_pulse/ScrollBox.jsx
+++ src/components/market_pulse/ScrollBox.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { createEditorState } from '../../utils/createEditorState';
 import { stateToHTML } from 'draft-js-export-html';
-import { convertFromRaw } from 'draft-js';
 
 export default function ReadMoreContent({ content }) {
   const [isScrollOn, setisScrollOn] = useState(false);
@@ -9,14 +8,13 @@ export default function ReadMoreContent({ content }) {
   const toggleExpand = () => {
     setisScrollOn(!isScrollOn);
   };
-
-  const contentState = convertFromRaw(content);
-  const editorState = createEditorState(contentState);
-
+  const editorState = createEditorState(content)
   const htmlContent = stateToHTML(editorState.getCurrentContent());
 
-    console.log(editorState);
-    
+  const isHtmlContentEmpty = (htmlString) => {
+    const cleaned = htmlString.replace(/<(\w+)[^>]*>(\s|<br\s*\/?>)*<\/\1>/g, '').trim();
+    return cleaned.length === 0;
+  }
 
   return (
     <div className="p-4 mx-auto">
@@ -27,13 +25,13 @@ export default function ReadMoreContent({ content }) {
             : 'max-h-20 overflow-hidden'
         }`}
       >
-        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
       </div>
       <button
         onClick={toggleExpand}
         className="mt-2 text-gold-light_400 hover:underline focus:outline-none"
       >
-        {content && (isScrollOn ? 'Read Less' : 'Read More')}
+        {!isHtmlContentEmpty(htmlContent) && (isScrollOn ? 'Read Less' : 'Read More')}
       </button>
     </div>
   );
